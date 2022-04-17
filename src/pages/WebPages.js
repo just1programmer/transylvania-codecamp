3cf07503a72121db50e9d7e8bcfb58cc7181fa45
Onofrei Sebastian <sebastianonofreiadrian@gmail.com>
2022-04-17 19:04:29 +0300

    changed structure of the routing part

--- src/pages/WebPages.js
+++ src/pages/WebPages.js
@@ -1,2 +1,20 @@
-import WhatIsJava from "./WhatIsJava";
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+import WhatIsJava from './WhatIsJava'
+
+const WebPages = () => {
+    return(
+        <Router>
+            <Route exact path="/" component= {Home} />
+            <Route path = "/what-is-java" component = {WhatIsJava} />
+            <Route path = "/favorites" component = {Favorites} />
+        </Router>
+    );
+};
+export default Webpages;
