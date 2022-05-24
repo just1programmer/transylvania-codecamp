e7c8b610074ad49749994dfc89ff2b01f78ffcc6
Sebastian <bastianonofrei@gmail.com>
2022-05-24 11:28:34 +0300

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
-const rootElem = document.querySelector("#root")
-
-ReactDOM.render(<App/>,rootElem)
